1fb4306d795612a11ba894598c76a8a4b300767f
Lucas Toledo <lucastoledo@Lucass-Mac-mini.local>
2021-12-24 14:17:04 -0300

    fix header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,8 +8,8 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class HeaderComponent {
 
-  @ViewChild('header')
-  private headerElement: ElementRef;
+  //@ViewChild('header')
+  //private headerElement: ElementRef;
 
   @ViewChild('navcontent')
   private content: ElementRef;
@@ -17,14 +17,14 @@ export class HeaderComponent {
   private isopen: boolean = false;
 
   constructor(private viewportScroller: ViewportScroller) {
-    document.addEventListener('scroll', (e) => {
+    /*document.addEventListener('scroll', (e) => {
       const topPosition = this.viewportScroller.getScrollPosition()[1];
       if (topPosition > 75) {
         this.headerElement.nativeElement.classList.add("gradient");
       } else {
         this.headerElement.nativeElement.classList.remove("gradient");
       }
-    })
+    })*/
   }
 
   scrollTop() {
